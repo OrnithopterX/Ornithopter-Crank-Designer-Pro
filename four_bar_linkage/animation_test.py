91bf4dbafb5f45d47fceefce487cc7587e1d2880
RCmags <memoryofatrufestival@gmail.com>
2022-08-08 16:58:01 -0500

    common dual crank

--- four_bar_linkage/animation_test.py
+++ four_bar_linkage/animation_test.py
@@ -5,18 +5,20 @@ from src.planar_configurations import *
 #---- test functions
 
 # input parameters
-RADIUS_INPUT  = 1      # crank
-RADIUS_OUTPUT = 2      # output lever/shoulder
-CONROD_LENGTH = 3      # linakge between crank and output
-Y_OUTPUT = 3           # vertical offset of output lever
-X_OUTPUT = 0.1         # horizontal offset of output lever
-X_INPUT  = 0.2         # horizontal offset of crank
-ANG_OFFSET = 1.2       # angular offset between cranks - staggered configuration 
+RADIUS_INPUT    = 1          # crank
+RADIUS_OUTPUT   = 2          # output lever/shoulder
+CONROD_LENGTH   = 3          # linkage between crank and output
+Y_OUTPUT        = 3          # vertical offset of output lever
+X_OUTPUT        = 0.1        # horizontal offset of output lever
+X_INPUT         = 0.2        # horizontal offset of crank
+ANG_OFFSET_DUAL = 0.55       # angular offset between cranks - staggered configuration 
+ANG_OFFSET_STAG = 1.2
 
 # display configurations
-anim1 = staggeredCrankAnim(RADIUS_INPUT, RADIUS_OUTPUT, CONROD_LENGTH, X_OUTPUT, Y_OUTPUT, ANG_OFFSET)
-anim2 = duelCrankAnim(RADIUS_INPUT, RADIUS_OUTPUT, CONROD_LENGTH, X_INPUT, X_OUTPUT, Y_OUTPUT)
-anim3 = insideOutCrankAnim(RADIUS_INPUT, RADIUS_OUTPUT, CONROD_LENGTH, X_OUTPUT, Y_OUTPUT)
+anim1 = staggeredCrankAnim(RADIUS_INPUT, RADIUS_OUTPUT, CONROD_LENGTH, X_OUTPUT, Y_OUTPUT, ANG_OFFSET_STAG)
+anim2 = duelCrankOppositeAnim(RADIUS_INPUT, RADIUS_OUTPUT, CONROD_LENGTH, X_INPUT, X_OUTPUT, Y_OUTPUT)
+anim3 = duelCrankCommonAnim(RADIUS_INPUT, RADIUS_OUTPUT, CONROD_LENGTH, X_INPUT, X_OUTPUT, Y_OUTPUT, ANG_OFFSET_DUAL)
+anim4 = insideOutCrankAnim(RADIUS_INPUT, RADIUS_OUTPUT, CONROD_LENGTH, X_OUTPUT, Y_OUTPUT)
 plt.show() 
 
 # Note: Run this script to visualize each configuration
--- four_bar_linkage/src/planar_configurations.py
+++ four_bar_linkage/src/planar_configurations.py
@@ -7,10 +7,16 @@ def staggeredCrankAnim(radius_input, radius_output, conrod, x_output, y_output, 
 	output = PlanarAnimation(radius_input, radius_output, conrod, 0, x_output, y_output, fcrank)
 	return output.animate()
 
-def duelCrankAnim(radius_input, radius_output, conrod, x_input, x_output, y_output):
+def duelCrankOppositeAnim(radius_input, radius_output, conrod, x_input, x_output, y_output):
 	fcrank = lambda x: -x
 	x_input += radius_input
 	output = PlanarAnimation(radius_input, radius_output, conrod, x_input, x_output, y_output, fcrank, flip_input=True)
+	return output.animate()
+
+def duelCrankCommonAnim(radius_input, radius_output, conrod, x_input, x_output, y_output, ang_offset):
+	fcrank = lambda x: x + ang_offset
+	x_input += radius_input
+	output = PlanarAnimation(radius_input, radius_output, conrod, x_input, x_output, y_output, fcrank)
 	return output.animate()
 	
 def insideOutCrankAnim(radius_input, radius_output, conrod, x_output, y_output):
